f742a7173b4d7e5b76eef7de310842eca1bc47cd
DucCong <89336151+KaitoKent@users.noreply.github.com>
2023-03-07 04:38:41 +0700

    ghep trang home

--- js/main.js
+++ js/main.js
@@ -1,49 +1,87 @@
-(function() {
-    var $slides = document.querySelectorAll('.slide');
-    var $controls = document.querySelectorAll('.slider__control');
-    var numOfSlides = $slides.length;
-    var slidingAT = 1300; // sync this with scss variable
-    var slidingBlocked = false;
-  
-    [].slice.call($slides).forEach(function($el, index) {
-      var i = index + 1;
-      $el.classList.add('slide-' + i);
-      $el.dataset.slide = i;
-    });
-  
-    [].slice.call($controls).forEach(function($el) {
-      $el.addEventListener('click', controlClickHandler);
-    });
-  
-    function controlClickHandler() {
-      if (slidingBlocked) return;
-      slidingBlocked = true;
-  
-      var $control = this;
-      var isRight = $control.classList.contains('m--right');
-      var $curActive = document.querySelector('.slide.s--active');
-      var index = +$curActive.dataset.slide;
-      (isRight) ? index++ : index--;
-      if (index < 1) index = numOfSlides;
-      if (index > numOfSlides) index = 1;
-      var $newActive = document.querySelector('.slide-' + index);
-  
-      $control.classList.add('a--rotation');
-      $curActive.classList.remove('s--active', 's--active-prev');
-      document.querySelector('.slide.s--prev').classList.remove('s--prev');
-      
-      $newActive.classList.add('s--active');
-      if (!isRight) $newActive.classList.add('s--active-prev');
-      
-  
-      var prevIndex = index - 1;
-      if (prevIndex < 1) prevIndex = numOfSlides;
-  
-      document.querySelector('.slide-' + prevIndex).classList.add('s--prev');
-  
-      setTimeout(function() {
-        $control.classList.remove('a--rotation');
-        slidingBlocked = false;
-      }, slidingAT*0.75);
-    };
-  }());
+const slideContainer = document.querySelector('.slider');
+const sliderText = document.querySelector('.slider--text');
+const btnLeft = document.querySelector('.slider__btn-left');
+const btnRight = document.querySelector('.slider__btn-right');
+
+const sliderImages = [
+  {
+    src: './image/1111055.png',
+    text: '22 December 2022'
+  },
+  {
+    src: './image/1111057.png',
+    text: '22 December 2022'
+  },
+  {
+    src: './image/1111059.png',
+    text: '22 December 2022'
+  }
+];
+
+let slideCounter = 0;
+
+const startSlider = () => {
+  slideContainer.style.backgroundImage = `linear-gradient(
+      to right,
+      rgba(34, 34, 34, 0.4),
+      rgba(68, 68, 68, 0.4)
+    ), url(${sliderImages[0].src})`;
+  sliderText.innerHTML = sliderImages[0].text;
+};
+
+btnRight.addEventListener('click', function() {
+  if (slideCounter === sliderImages.length - 1) {
+    slideContainer.style.backgroundImage = `linear-gradient(
+      to right,
+      rgba(34, 34, 34, 0.4),
+      rgba(68, 68, 68, 0.4)
+    ), url(${sliderImages[0].src})`;
+    sliderText.innerHTML = sliderImages[0].text;
+    slideCounter = -1;
+
+    slideContainer.classList.add('fadeIn');
+    setTimeout(() => {
+      slideContainer.classList.remove('fadeIn');
+    }, 1000);
+  }
+  slideContainer.style.backgroundImage = `linear-gradient(
+      to right,
+      rgba(34, 34, 34, 0.4),
+      rgba(68, 68, 68, 0.4)
+      ),url(${sliderImages[slideCounter + 1].src})`;
+  sliderText.innerHTML = sliderImages[slideCounter + 1].text;
+  slideCounter++;
+  slideContainer.classList.add('fadeIn');
+  setTimeout(() => {
+    slideContainer.classList.remove('fadeIn');
+  }, 1000);
+});
+
+btnLeft.addEventListener('click', function() {
+  if (slideCounter === 0) {
+    slideContainer.style.backgroundImage = `linear-gradient(
+      to right,
+      rgba(34, 34, 34, 0.4),
+      rgba(68, 68, 68, 0.4)
+    ),url(${sliderImages[sliderImages.length - 1].src})`;
+    sliderText.innerHTML = sliderImages[sliderImages.length - 1].text;
+    slideCounter = sliderImages.length;
+    slideContainer.classList.add('fadeIn');
+    setTimeout(() => {
+      slideContainer.classList.remove('fadeIn');
+    }, 1000);
+  }
+
+  slideContainer.style.backgroundImage = `linear-gradient(
+      to right,
+      rgba(34, 34, 34, 0.4),
+      rgba(68, 68, 68, 0.4)
+    ),url(${sliderImages[slideCounter - 1].src})`;
+  sliderText.innerHTML = sliderImages[slideCounter - 1].text;
+  slideCounter--;
+  slideContainer.classList.add('fadeIn');
+  setTimeout(() => {
+    slideContainer.classList.remove('fadeIn');
+  }, 1000);
+});
+document.addEventListener('DOMContentLoaded', startSlider);
